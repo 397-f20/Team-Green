98dc5b41722e046b0935f70e4de0b950dc12e2c5
Atul Jalan <atuljalan17@gmail.com>
2020-10-26 17:23:13 -0500

    added test file for timer

---
+++ __tests__/Timer.test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import { fireEvent, render, waitFor } from 'react-native-testing-library';
+import {expect, it} from '@jest/globals';
+import Timer from '../src/Timer/Timers.js';
+
+describe('Timer', () => {
+  test('Timer counts down correctly', () => {
+    
+  })
+});
