50e03fe2b83ad5a66d25fa3073b25422a5a47dbc
JennyTLam <jennylam2021@u.northwestern.edu>
2020-10-28 03:37:29 -0500

    Added profile screen tests

---
+++ __tests__/Profile.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { render } from '@testing-library/react-native'; 
+import { expect, it, done } from '@jest/globals';
+import Profile from '../src/Profile/Profile';
+import ProfileHeader from '../src/Profile/ProfileHeader';
+
+
+
+describe('Profile', () => {
+  it('should render Profile User Bar', () => {
+    render(<ProfileHeader />);
+  })
+
+  it('should render Profile Chart', () => {
+    render(<Profile />);
+  })
+
+  it('should show username text', () => {
+    const username = "Shaggy Rogers";
+    const header = render(<ProfileHeader title={username} img={"https://randomuser.me/api/portraits/men/1.jpg"} />);
+    const title = header.queryByText(username);
+    expect(title).toBeTruthy();
+    //expect(header.props.title).not.toBeNaN();
+  })
+
+  it('should show progress cycle chart', () => {
+    const profile = render(<Profile />);
+    const graphDisplayed = profile.queryByText(' Studying Progress (cycles) ');
+    expect(graphDisplayed).toBeTruthy();
+  })
+});
