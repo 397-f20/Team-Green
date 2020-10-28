465261e3776f4de2c36769081b342577a532761e
Jing Jiang <jasperjiang97@gmail.com>
2020-10-28 08:10:52 -0500

    update social page test3

--- __tests__/Social.test.js
+++ __tests__/Social.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { render } from '@testing-library/react-native'; 
 import { expect, it, done } from '@jest/globals';
 
-import Social from '../src/Social/Social';
 import Background from '../src/FishTank/Background';
 import Dropdown from '../src/Social/Dropdown';
 
 
 
 describe("Social Page Testing", () => {
-    it('should render Background', async() => {
+    it('should render Background', () => {
         const fishObj = {
             'fishObj': {
                 idx: 0,
                 size: 50
             }
-        }
-        const background = render(<Background fishObjects={fishObj} />);
-        expect(background).toBeDefined();
-    })
+        };
+        async() => {
+            render(<Background fishObjects={fishObj} />)
+        };
+    });
 
     // In Progress
     // it('selecting user updates number of fish', () => {
