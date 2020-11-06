d3c28d65433cc323eb31cfc32ea502d35741149b
JennyTLam <jennylam2021@u.northwestern.edu>
2020-11-05 22:09:32 -0600

    Updated timer tests

--- __tests__/Timer.test.js
+++ __tests__/Timer.test.js
@@ -8,23 +8,49 @@ jest.useFakeTimers();
 
 describe('Timer', () => {
   it('Timer counts down correctly', () => {
-    const timer = render(<Timer />)
-    const start = timer.queryByText('Start')
-    fireEvent.press(start);
+    async() => {
+      const timer = render(<Timer />)
+      const start = timer.queryByText('Start')
+      fireEvent.press(start);
 
-    for (let i = 10; i > 1; i--) {
-      let renderedTime = '00:' + i.toString();
-      if (i < 10) {
-        renderedTime = '00:0' + i.toString();
+      for (let i = 10; i > 1; i--) {
+        let renderedTime = '00:' + i.toString();
+        if (i < 10) {
+          renderedTime = '00:0' + i.toString();
+        }
+        let timeIsThere = timer.queryByText(renderedTime);
+        expect(timeIsThere).toBeTruthy();
+        act(() => jest.runOnlyPendingTimers());
       }
-      let timeIsThere = timer.queryByText(renderedTime);
-      expect(timeIsThere).toBeTruthy();
-      act(() => jest.runOnlyPendingTimers());
     }
   })
+
   it('Timer displays time correctly', () => {
-    const timer = render(<Timer />)
-    const start = timer.queryByText('Start');
-    expect(start).toBeTruthy();
+    async() =>{
+      const timer = render(<Timer />)
+      const start = timer.queryByText('Start');
+      expect(start).toBeTruthy();
+    }
+  })
+
+  it('should restart timer on restart', () => {
+    async () => {
+      const timer = render(<Timer />)
+    
+      const start = timer.queryByText('Start')
+      const restart = timer.queryByText('Restart')
+      fireEvent.press(start);
+      act(() => jest.runOnlyPendingTimers());
+
+      let ranTime = '00:10';
+      let timeIsThere = timer.queryByText(ranTime);
+      expect(timeIsThere).toBeTruthy();
+
+      fireEvent.press(restart)
+
+      let startTime = '25:00';
+      let startTimeThere = timer.queryByText(startTime);
+      expect(startTimeThere).toBeTruthy();
+    }
   })
 });
