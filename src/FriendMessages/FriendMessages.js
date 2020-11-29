d9fdaa825706a5d13147e2eccecd7b2282d2342a
Atul Jalan <atuljalan17@gmail.com>
2020-11-29 12:00:51 -0800

    styled message component

--- src/FriendMessages/FriendMessages.js
+++ src/FriendMessages/FriendMessages.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import BackButton from "./Back";
 import { useUserContext } from '../UserContext';
 import Message from "./Message";
@@ -12,7 +12,8 @@ const FriendMessages = ({ route, navigation }) => {
 
   const ReceivedMessages = () =>{
     return (
-      <View>
+      <View style={{width: '100%', justifyContent: 'center', alignItems: 'flex-start', marginBottom: 20}}>
+        <Text style={styles.sectionHeader}>Received messages</Text>
         {"messages" in userData && "received" in userData.messages && Object.keys(userData.messages.received).length > 0 ? 
           Object.values(userData.messages.received).map( msg => (<Message msg={msg} type="From" />))  :
           <Text>No messages sent!</Text>
@@ -23,7 +24,8 @@ const FriendMessages = ({ route, navigation }) => {
 
   const SentMessages = () =>{
     return (
-      <View>
+      <View style={{width: '100%', justifyContent: 'center', alignItems: 'flex-start'}}>
+        <Text style={styles.sectionHeader}>Sent messages</Text>
         {"messages" in userData && "sent" in userData.messages && Object.keys(userData.messages.sent).length > 0 ? 
           Object.values(userData.messages.sent).map( msg => (<Message msg={msg} type="To" />)) :
           <Text>No messages sent!</Text>
@@ -35,10 +37,20 @@ const FriendMessages = ({ route, navigation }) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <BackButton navigation={navigation}/>
-      <ReceivedMessages />
-      <SentMessages />      
+      <View style={{width: '50%'}}>
+        <ReceivedMessages />
+        <SentMessages />  
+      </View>    
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  sectionHeader: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 5
+  }
+})
 
 export default FriendMessages;
--- src/FriendMessages/Message.js
+++ src/FriendMessages/Message.js
@@ -4,7 +4,14 @@ import { View, Text, StyleSheet} from 'react-native';
 const Message = ({msg, type}) => {
     return(
       <View style={styles.container}>
-        <Text style={styles.text}> {type}: {msg.sender} {msg.message} {msg.timestamp} </Text>
+        <View>
+          <View style={styles.leftBar} />
+          <View style={styles.metaData}>
+            <Text style={{marginRight: 20, fontSize: 12, fontWeight: '700', color: 'gray'}}>{type === 'From' ? msg.sender : msg.to}</Text>
+            <Text style={{fontWeight: '600', fontSize: 12, color: 'rgb(0, 164, 228)'}}>{msg.timestamp}</Text>
+          </View>
+          <Text style={styles.text}>{msg.message}</Text>
+        </View>
       </View>
     )
 }
@@ -14,8 +21,23 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         flexDirection: 'column',
-        alignItems: 'left', 
-        marginLeft: 20
+        alignItems: 'flex-start', 
+        marginLeft: 20,
+        marginBottom: 10
+    },
+    leftBar: {
+      height: '100%',
+      width: 3,
+      backgroundColor: 'rgb(0, 164, 228)',
+      position: 'absolute',
+      top: 0,
+      left: -10,
+      borderRadius: 10
+    },
+    metaData: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
     },
     text: {
         fontSize: 18,
