5224d7c30dc3f5767ca754b0011a3150848a8a09
JennyTLam <jennylam2021@u.northwestern.edu>
2020-11-30 20:22:27 -0600

    cypress

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -5,17 +5,24 @@ describe ('Test App', () => {
 
   it('logs in', () => {
     cy.visit('/');
+    cy.wait(3000)
     const email = "claire@gmail.com";
     const password = "123456";
     cy.get('input').eq(0).type(email).should('have.value', email);
     cy.get('input').eq(1).type(password).should('have.value', password);
     cy.contains("Sign in").click()
 
+    cy.wait(3000);
     cy.contains("Timer").click()
     cy.contains("Start").click()
     cy.contains("Pause")
-
-    cy.contains("Social").click()
-    cy.contains("(You)").click()
   })
+
+  it('when-then: when user click dropdown, see a friend list and navigate to fishtank of friend', () => {
+    cy.visit('/');
+    cy.wait(3000);
+    cy.contains("Tank").click();
+    cy.contains("(You)").click();
+    cy.contains("Atul").click();
+});
 });
